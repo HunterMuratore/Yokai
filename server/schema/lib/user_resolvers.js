c1caf31c9efc3b87d3edd094eefd6dded4cc2a22
Hunter <muratoreh@gmail.com>
2023-11-16 15:35:46 -0500

    proxy localhost

--- server/schema/lib/user_resolvers.js
+++ server/schema/lib/user_resolvers.js
@@ -1,2 +1,3 @@
-const User = require('../../models/User')
-const WishList = require('../../models/WishList')
+const User = require('../../models/User');
+const WishList = require('../../models/WishList');
+const Product = require('../../models/Product');
