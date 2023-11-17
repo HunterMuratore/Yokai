9416ee0d51ce6389b88f6a4282ec4f22fde1468b
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-11-17 16:42:46 -0500

    working on frontend

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,34 @@
+import React from 'react';
+
 function Footer() {
+    const date = new Date();
+
     return (
         <>
-            <footer>
-                <div>footer</div>
+
+            <hr className="divider"></hr>
+
+            <footer className="footer m-2 text-center">
+                <p className="copyright mb-1">&copy; {date.getFullYear()}</p>
+                <p className="mb-1">
+                    <span className="d-none d-sm-inline">Developed by Group 2</span>
+                    <span className="d-inline d-sm-none">Dev'd by Group 2</span>
+                </p>
+                <div className="d-flex align-items-center mb-1">
+                    <a href="https://github.com/Kev-Castro" target="_blank" rel="noopener noreferrer" className="text-dark me-3">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="https://github.com/Kev-Castro" target="_blank" rel="noopener noreferrer" className="text-dark me-3">
+                        <i className="fab fa-facebook"></i>
+
+                    </a>
+                    <a href="https://github.com/Kev-Castro" target="_blank" rel="noopener noreferrer" className="text-dark ">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                </div>
             </footer>
         </>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
