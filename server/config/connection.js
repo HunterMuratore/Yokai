63d3fa1b426d09f89d2e8f96b5737bb2aca1f206
Hunter <muratoreh@gmail.com>
2023-11-22 10:25:14 -0500

    removed cache

--- server/config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const is_prod = process.env.PORT;
-
-mongoose.connect(is_prod ? process.env.DB_URL : 'mongodb://127.0.0.1:27017/wishlist_db');
-
-
-module.exports = mongoose.connection;
